713f88a2abaebd46f539af023f8f3a04dad54092
fakebusiness <weberth6@students.zhaw.ch>
2024-03-26 13:15:24 +0100

    added springboot application campcompass

---
+++ campcompass/src/main/java/ch/zhaw/campcompass/CampcompassApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.campcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampcompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampcompassApplication.class, args);
+	}
+
+}
